85d5bd8eb9995c94846d45e90c4664aa914f7365
rincastle <if22.rindiseptiani@mhs.ubpkarawang.ac.id>
2024-05-11 16:35:57 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BSAFP',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () => import('./authentication/sign-in/sign-in.module').then( m => m.SignInPageModule)
+  },
+  {
+  path: 'sign-up',
+    loadChildren: () => import('./authentication/sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
